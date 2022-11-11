c2d17df392ac631f0def8dafd3a14144a3f56951
Lindsay Oliver <loliver@darwin.camhres.ca>
2022-11-11 11:44:00 -0500

    Added scripts to parse EA task tsv files

---
+++ code/parse_EA_task_tsv_to_AFNI_format_2runs_ZHP.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#SBATCH --partition=low-moby
+#SBATCH --array=1-26
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#BSATCH --mem-per-cpu=1G
+#SBATCH --time=1:00:00
+#SBATCH --export=ALL
+#SBATCH --job-name="AFNI_convert_task_1D_ZHP"
+#SBATCH --output=/projects/loliver/ModSoCCS/data/logs/parse_EA_task_tsv_to_AFNI_format_ZHP_%j.txt
+
+# this sublist includes the full directory name # ZHP participants with timing issue only
+sublist="/projects/loliver/ModSoCCS/data/processed/EA_task_1D_sublist_ZHP.txt"
+index () {
+	  head -n $SLURM_ARRAY_TASK_ID $sublist \
+	  | tail -n 1
+	 }
+
+sub_indir="/archive/data/SPN20/data/nii"
+sub_outdir="/projects/loliver/ModSoCCS/data/processed/"
+full=`index`
+site=$(echo $full| cut -c 7-9)
+subnum=$(echo $full| cut -c 11-14)
+subj="$site$subnum"
+bids="sub-$site$subnum"
+ses=$(echo $full| cut -c 16-17)
+
+python3 /projects/loliver/ModSoCCS/code/parse_EA_task_tsv_to_AFNI_format_2runs_ZHP.py ${sub_indir}/`index` ${subj} ${sub_outdir}/${bids}/ses-${ses}
+
